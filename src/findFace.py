8dea6b26fbb0d6f01198a5bb9a66694d08f0a916
DylanMoeller <u16dm21@abdn.ac.uk>
2024-02-24 13:41:57 +0000

    refactoring

--- src/findFace.py
+++ src/findFace.py
@@ -1,24 +1,25 @@
 import numpy as np
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier('../Cascades/haarcascade_frontalface_default.xml') #From OpenCV (not my own training file)
+face_cascade = cv.CascadeClassifier(
+    '../Cascades/haarcascade_frontalface_default.xml')  # From OpenCV (not my own training file)
 capture = cv.VideoCapture(0)
-capture.set(3, 500) #Width and height of camera frame
-capture.set(4,500)
+capture.set(3, 500)  # Width and height of camera frame
+capture.set(4, 500)
 
 while True:
     ret, frame = capture.read()
-    frame = cv.flip(frame,1)
+    frame = cv.flip(frame, 1)
     faces = face_cascade.detectMultiScale(
         frame,
         scaleFactor=1.2,
-        minNeighbors=25,
+        minNeighbors=5,  # Higher this is, lower false positives
         minSize=(20, 20)
     )
 
     for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0),10)
-        color = frame[y:y+h, x:x+w]
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 10)
+        color = frame[y:y + h, x:x + w]
 
     cv.imshow('frame', frame)
 
