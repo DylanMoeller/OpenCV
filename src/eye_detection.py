1890466e5b088206aab78e2c4fff690d719b25ae
DylanMoeller <u16dm21@abdn.ac.uk>
2024-02-24 14:16:44 +0000

    eye detection file accuracy is not perfect (always detects eyes but also adds other facial features

--- src/eye_detection.py
+++ src/eye_detection.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2 as cv
 
+face_cascade = cv.CascadeClassifier('../Cascades/haarcascade_frontalface_default.xml')  # From OpenCV (not my own training file)
 eye_cascade = cv.CascadeClassifier('../Cascades/haarcascade_eye.xml')
-
 capture = cv.VideoCapture(0)
 capture.set(3, 500)  # Width and height of camera frame
 capture.set(4, 500)
@@ -10,21 +10,28 @@ capture.set(4, 500)
 while True:
     ret, frame = capture.read()
     frame = cv.flip(frame, 1)
-    eyes = eye_cascade.detectMultiScale(
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
         frame,
         scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(30, 30)
+        minNeighbors=25,  # Higher this is, lower false positives
+        minSize=(20, 20)
     )
-    for (x, y, w, h) in eyes:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 10)
-        color = frame[y:y+h, x:x+w]
 
-        cv.imshow('Eye', frame)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 10)
+        color = frame[y:y + h, x:x + w]
+        gray = frame[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255),2)
 
-        k = cv.waitKey(30) & 0xff
-        if k == 27:
-            break
 
-    capture.release()
-    cv.destroyAllWindows()
+    cv.imshow('frame', frame)
+
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+capture.release()
+cv.destroyAllWindows()
