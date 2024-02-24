b93fc8d869a690fd8fd4f7c9bdb251bd00f963e6
DylanMoeller <u16dm21@abdn.ac.uk>
2024-02-24 13:37:37 +0000

    refactoring

--- src/findFace.py
+++ src/findFace.py
@@ -9,18 +9,19 @@ capture.set(4,500)
 while True:
     ret, frame = capture.read()
     frame = cv.flip(frame,1)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=10,
-        minSize=(1, 1)
+        frame,
+        scaleFactor=1.2,
+        minNeighbors=25,
+        minSize=(20, 20)
     )
-    for (x,y,w,h) in faces:
-        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-    cv.imshow('frame',frame)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0),10)
+        color = frame[y:y+h, x:x+w]
+
+    cv.imshow('frame', frame)
+
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
